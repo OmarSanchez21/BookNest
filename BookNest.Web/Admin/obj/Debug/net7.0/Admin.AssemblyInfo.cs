67ea392e28d5a17f5f8dcdcc3397b2eb135e1ba8
Omar Sanchez <omisp21@gmail.com>
2024-08-17 22:43:42 -0400

    libor model

--- BookNest.Web/Admin/obj/Debug/net7.0/Admin.AssemblyInfo.cs
+++ BookNest.Web/Admin/obj/Debug/net7.0/Admin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Admin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cd9406d7c5f1ffce8e821743d5eec5e62f5ed1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f52d88ab80f3f49baea720c62f7a338d2b5627d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Admin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Admin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
